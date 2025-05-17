1689d8626933b1ac386a16d842c84c41b02ad4d7
mansamusa <ajikobilanre007@gmail.com>
2025-05-17 17:29:05 +0100

    feat: add addComment mutation for posting comments and notifications

---
+++ convex/comments.ts
@@ -0,0 +1,45 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { getAuthenticatedUser } from "./users";
+
+
+export const addComment = mutation({
+  args: {
+    content: v.string(),
+    postId: v.id("posts"),
+  },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    const post = await ctx.db.get(args.postId);
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+
+    const commentId = await ctx.db.insert("comments",
+      {
+        content: args.content,
+        postId: post._id,
+        userId: currentUser?._id,
+      },
+    );
+
+    // increment the post's comments count by 1
+    await ctx.db.patch(post._id, { comments: post.comments + 1 });
+    // create a notification if the user is not the post author
+    if (currentUser._id !== post.userId) {
+      await ctx.db.insert("notifications", {
+        recieverId: post.userId,
+        senderId: currentUser._id,
+        type: "comment",
+        postId: args.postId,
+        commentId,
+      });
+    }
+
+
+    return commentId;
+  },
+});
+
